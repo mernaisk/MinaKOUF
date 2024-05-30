a1b599883a22c0f27afab7f641bd75dd9b30c333
Rafael Youakeem <youakeem@gmail.com>
2024-05-30 17:26:58 +0200

    Setup Tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,29 @@
+import "../tamagui-web.css";
+
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { TamaguiProvider } from "tamagui";
+
+import { tamaguiConfig } from "../tamagui.config";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <TamaguiProvider
+      config={tamaguiConfig}
+      defaultTheme={colorScheme ?? "light"}
+    >
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import "@tamagui/core/reset.css";
+import { useState } from "react";
+import { Button, H1, View, XStack } from "tamagui";
 
 export default function Index() {
+  const [count, setCount] = useState(0);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <XStack gap="$2" padding="$5">
+        <Button theme="green" onPress={() => setCount(count + 1)} flex={1}>
+          Increment
+        </Button>
+        <Button theme="orange" onPress={() => setCount(count - 1)} flex={1}>
+          Decrement
+        </Button>
+      </XStack>
+
+      <H1 textAlign="center">{count}</H1>
     </View>
   );
 }
